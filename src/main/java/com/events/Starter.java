fb8d1c225b570b60b88cf07f3206ae3f82586f24
akash12011 <akash.kumar1@oyorooms.com>
2018-10-18 17:00:51 +0530

    project structure

---
+++ src/main/java/com/events/Starter.java
@@ -0,0 +1,17 @@
+package com.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@EnableAsync
+@EnableScheduling
+public class Starter {
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(Starter.class, args);
+    }
+}
